ccb0914d1d5484100f1ce94b363f53eabd903c57
FreakingBarbarians <Paarthurnax@live.ca>
2017-12-08 13:31:20 -0500

    Started Debug Editor UI

---
+++ Assets/Code/UI/CursorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorManager : MonoBehaviour {
+	public static CursorManager instance;
+	public Sprite CursorTex;
+	public Image CursorImage;
+	public float CURSOR_BASE_SCALE;
+
+	// Use this for initialization
+	void Start () {
+		if (instance) {
+			Debug.LogWarning ("Multiple Cursor Manager Instances");
+			return;
+		}
+
+		instance = this;
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
+		CURSOR_BASE_SCALE = Screen.width / 64;
+		transform.localScale = new Vector3 (CURSOR_BASE_SCALE, CURSOR_BASE_SCALE, 1);
+		CursorImage.sprite = CursorTex;
+	}
+
+	void FixedUpdate () {
+		transform.position = Input.mousePosition;	
+	}
+}
---
+++ Assets/PartPicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartPicker : MonoBehaviour {
+
+	public List<GameObject> Source;
+
+	public int xPadding;
+	public int yPadding;
+
+	public int xEndPaddding;
+	public int yEndPadding;
+
+	public int ElementSize;
+	public int ElementWidth;
+
+	public GameObject ElementPrefab;
+}
